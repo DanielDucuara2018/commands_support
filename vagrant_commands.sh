49965e37afe0535f22c5947120387166b74c815b
Daniel Ducuara <38841094+DanielDucuara2018@users.noreply.github.com>
2021-05-07 18:52:02 +0200

    Create vagrant_commands.sh

---
+++ vagrant_commands.sh
@@ -0,0 +1,11 @@
+vagrant init # Create a configuration template file (Vagrantfile)
+vagrant up # Run a VM
+vagrant reload # reload a VM after make changes
+vagrant ssh # ssh access to a VM
+vagrant box list # list of VMs 
+vagrant box remove # remove a VM
+vagrant global-status # list of running VMs
+vagrant destroy # run in the VM folder. Result: VM is stopped and deleted
+
+vagrant plugin install vagrant-docker
+vagrant plugin install vagrant-docker-compose # docker compose installation
